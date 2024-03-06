b4b4ffa988638435a59ba692947b45964619e9de
Gabriel Ferreira Carvalho <118980722+gabrieloakz@users.noreply.github.com>
2024-03-06 16:49:51 +0000

    initial
    deleted old files

--- JS/animations.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        document.querySelector('.container').classList.remove('hidden');
-        document.querySelector('.header').classList.remove('hidden');
-        document.querySelector('.content').classList.remove('hidden');
-        document.querySelector('.footer').classList.remove('hidden');
-    }, 500);
-});
